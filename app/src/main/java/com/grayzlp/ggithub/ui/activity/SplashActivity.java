3cd47db3036b099137c50e745c7403b3b8979df1
grayzlp <grayzlp@126.com>
2017-09-08 01:11:34 +0800

    add splash activity

---
+++ app/src/main/java/com/grayzlp/ggithub/ui/activity/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.grayzlp.ggithub.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.grayzlp.ggithub.R;
+import com.grayzlp.ggithub.util.AnimUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends Activity {
+
+    @BindView(R.id.branch) TextView branch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_screen);
+        ButterKnife.bind(this);
+        animateBranch();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkLogIn();
+    }
+
+    private void checkLogIn() {
+        // TODO Fix this after complete the module of api
+    }
+
+    private void animateBranch() {
+        branch.setAlpha(0f);
+        branch.setScaleX(0.8f);
+
+        branch.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .setStartDelay(300)
+                .setDuration(500)
+                .setInterpolator(AnimUtils.getFastOutLinearInInterpolator(this));
+
+
+    }
+}
