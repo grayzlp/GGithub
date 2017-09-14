91ee700389ab0cbbe864ce580f2eb80d32b1cb85
grayzlp <grayzlp@126.com>
2017-09-14 22:30:21 +0800

    fix drawer menu icon

--- app/src/main/java/com/grayzlp/ggithub/ui/activity/HomeActivity.java
+++ app/src/main/java/com/grayzlp/ggithub/ui/activity/HomeActivity.java
@@ -46,7 +46,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setupNavagationHeader();
         setupToolbar();
